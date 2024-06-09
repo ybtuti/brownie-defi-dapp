4e3309adb899fc7157194e9710f15998b4d3a7a9
0xodus <samuelmsuu@gmail.com>
2024-06-09 13:12:08 +0300

    chore: Update deploy.py to fix token transfer amount calculation

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,14 +2,15 @@ from scripts.helpful_scripts import get_account, get_contract
 from brownie import DappToken, TokenFarm, config, network
 from web3 import Web3
 
-KEPT_BALANCE = Web3.toWei(100, "ether")
+KEPT_BALANCE = Web3.to_wei(100, 'ether')
 
 
 def deploy_token_farm_and_dapp_token():
     account = get_account()
     dapp_token = DappToken.deploy({"from": account})
     token_farm = TokenFarm.deploy(dapp_token.address, {"from": account}, publish_source=config["networks"][network.show_active()]["verify"])
-    tx = dapp_token.transfer(token_farm.address, dapp_token.totalSupply(), - KEPT_BALANCE, {"from": account})
+    amount_to_transfer = dapp_token.totalSupply() - KEPT_BALANCE
+    tx = dapp_token.transfer(token_farm.address, amount_to_transfer, {"from": account})
     tx.wait(1)
     # dapp_token, weth_token, fau_token/dai
     weth_token = get_contract("weth_token")
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -2,15 +2,16 @@ from brownie import (
     network,
     accounts,
     config,
+    interface,
     LinkToken,
     MockV3Aggregator,
-    MockDAI,
     MockWETH,
-    MockOperator,
+    MockDAI,
     Contract,
-    web3,
 )
-import time
+
+INITIAL_PRICE_FEED_VALUE = 2000000000000000000000
+DECIMALS = 18
 
 NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS + [
@@ -19,27 +20,12 @@ LOCAL_BLOCKCHAIN_ENVIRONMENTS = NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS + [
     "matic-fork",
 ]
 
-# Etherscan usually takes a few blocks to register the contract has been deployed
-BLOCK_CONFIRMATIONS_FOR_VERIFICATION = (
-    1 if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS else 6
-)
-
 contract_to_mock = {
-    "fau_token": MockDAI,
-    "dai_usd_price_feed": MockV3Aggregator,
     "eth_usd_price_feed": MockV3Aggregator,
+    "dai_usd_price_feed": MockV3Aggregator,
+    "fau_token": MockDAI,
     "weth_token": MockWETH,
-    "operator": MockOperator,
 }
-
-DECIMALS = 18
-INITIAL_VALUE = web3.toWei(2000, "ether")
-BASE_FEE = 100000000000000000  # The premium
-GAS_PRICE_LINK = 1e9  # Some value calculated depending on the Layer 1 cost and Link
-
-
-def is_verifiable_contract() -> bool:
-    return config["networks"][network.show_active()].get("verify", False)
 
 
 def get_account(index=None, id=None):
@@ -54,19 +40,19 @@ def get_account(index=None, id=None):
 
 def get_contract(contract_name):
     """If you want to use this function, go to the brownie config and add a new entry for
-    the contract that you want to be able to 'get'. Then add an entry in the variable 'contract_to_mock'.
+    the contract that you want to be able to 'get'. Then add an entry in the in the variable 'contract_to_mock'.
     You'll see examples like the 'link_token'.
         This script will then either:
             - Get a address from the config
             - Or deploy a mock to use for a network that doesn't have it
 
         Args:
-            contract_name (string): This is the name that is referred to in the
+            contract_name (string): This is the name that is refered to in the
             brownie config and 'contract_to_mock' variable.
 
         Returns:
             brownie.network.contract.ProjectContract: The most recently deployed
-            Contract of the type specificed by the dictionary. This could be either
+            Contract of the type specificed by the dictonary. This could be either
             a mock or the 'real' contract on a live network.
     """
     contract_type = contract_to_mock[contract_name]
@@ -95,16 +81,23 @@ def fund_with_link(
 ):
     account = account if account else get_account()
     link_token = link_token if link_token else get_contract("link_token")
-    ### Keep this line to show how it could be done without deploying a mock
-    # tx = interface.LinkTokenInterface(link_token.address).transfer(
-    #     contract_address, amount, {"from": account}
-    # )
-    tx = link_token.transfer(contract_address, amount, {"from": account})
+    tx = interface.LinkTokenInterface(link_token).transfer(
+        contract_address, amount, {"from": account}
+    )
     print("Funded {}".format(contract_address))
     return tx
 
 
-def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
+def get_verify_status():
+    verify = (
+        config["networks"][network.show_active()]["verify"]
+        if config["networks"][network.show_active()].get("verify")
+        else False
+    )
+    return verify
+
+
+def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_PRICE_FEED_VALUE):
     """
     Use this script if you want to deploy mocks to a testnet
     """
@@ -118,42 +111,30 @@ def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
         decimals, initial_value, {"from": account}
     )
     print(f"Deployed to {mock_price_feed.address}")
-    print("Deployihg Mock DAI ...")
+    print("Deploying Mock DAI...")
     dai_token = MockDAI.deploy({"from": account})
-    print("Deployed to {dai_token.address}")
-    print("Deploying Mock WETH ...")
+    print(f"Deployed to {dai_token.address}")
+    print("Deploying Mock WETH")
     weth_token = MockWETH.deploy({"from": account})
     print(f"Deployed to {weth_token.address}")
-    print("Mocks Deployed!")
+    
+# We didn't have this in the video, but it's a helpful script to have you issue the tokens!
+def issue_tokens():
+    """You can call this function once you have deployed your TokenFarm contract to a live network
+    and have users that have staked tokens.
 
-def listen_for_event(brownie_contract, event, timeout=200, poll_interval=2):
-    """Listen for an event to be fired from a contract.
-    We are waiting for the event to return, so this function is blocking.
+    Note that it relies on get_contract, so be mindful to correctly configure your Token Farm contract
+    into brownie-config.yaml as well as the contract_to_mock dict as described in the get_contract docstring
 
-    Args:
-        brownie_contract ([brownie.network.contract.ProjectContract]):
-        A brownie contract of some kind.
+    Run this function with this command: `brownie run scripts/issue_tokens.py --network kovan`
 
-        event ([string]): The event you'd like to listen for.
-
-        timeout (int, optional): The max amount in seconds you'd like to
-        wait for that event to fire. Defaults to 200 seconds.
-
-        poll_interval ([int]): How often to call your node to check for events.
-        Defaults to 2 seconds.
+        This function will:
+            - Print your account address and deployed TokenFarm contract address to confirm that you're using the right ones
+            - Call issueTokens on your deployed TokenFarm contract to issue the DAPP token reward to your users
     """
-    web3_contract = web3.eth.contract(
-        address=brownie_contract.address, abi=brownie_contract.abi
-    )
-    start_time = time.time()
-    current_time = time.time()
-    event_filter = web3_contract.events[event].createFilter(fromBlock="latest")
-    while current_time - start_time < timeout:
-        for event_response in event_filter.get_new_entries():
-            if event in event_response.event:
-                print("Found event!")
-                return event_response
-        time.sleep(poll_interval)
-        current_time = time.time()
-    print("Timeout reached, no event found.")
-    return {"event": None}
+    account = get_account()
+    print(f"Issue Tokens called by: {account}")
+    token_farm = get_contract("TokenFarm")
+    print(f"TokenFarm contract called to issue tokens: {token_farm}")
+    tx = token_farm.issueTokens({"from": account})
+    tx.wait(1)
