0cc66aed5c82d5997a502267b79b0ab273caa617
ybtuti <samuelmsuu@gmail.com>
2024-07-12 23:06:56 +0300

    refactor: Update networkName assignment in Main component

--- front_end/src/components/Main.tsx
+++ front_end/src/components/Main.tsx
@@ -1,5 +1,16 @@
 import { useEthers } from '@usedapp/core';
-import helperConfig from '../helper-config.json';
+//import helperConfig from '../helper-config.json';
+import networkMapping from "../chain-info/deployments/map.json"
+import { constants } from 'ethers';
+interface HelperConfig {
+    [key: number]: { name: string };
+
+}
+const helperConfig: HelperConfig = {
+    1337: { name: "Local Network" },
+    11155111: { name: "Some Network" },
+    1: { name: "Main Network" }
+};
 export const main = () => {
     //show token values
 
@@ -7,8 +18,10 @@ export const main = () => {
     //get balance of the users wallet
     //send the brownie-config to our `src` folder
     //send the build folder
-    const { chainId } = useEthers();
-    const networkName = chainId ? helperConfig[chainId].name : 'garnache';
-    //const dappTokenAddress
+    const { chainId, error } = useEthers();
+    const networkName = chainId ? helperConfig[chainId] : 'garnache';
+
+
+    const dappTokenAddress = chainId ? networkMapping[String(chainId)]["DappToken"][0] : constants.AddressZero
     return (<div>Hi!</div>)
 }
